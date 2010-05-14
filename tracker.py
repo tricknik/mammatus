4435e16c570524986d89bde36eae8ec6b35a239c
Dmytri Kleiner <dk@telekommunisten.net>
2010-05-14 17:43:33 +0200

    better

--- tracker.py
+++ tracker.py
@@ -1,26 +1,48 @@
 from twisted.internet.protocol import Factory, Protocol
-from twisted.internet import reactor
+from twisted.internet import reactor, defer
 from twisted.names import dns 
 from twisted.names.dns import RRHeader, Record_A, A, IN
-from twisted.names import client, server
+from twisted.names import common, client, server
 
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('telekommunisten.org', 0)) 
-ImaIPAddr = s.getsockname()[0] '
+ImaIPAddr = s.getsockname()[0]
 ImaRecordA = Record_A(ImaIPAddr,'1')
 ImaRR = RRHeader(name='mammacloud',type=A,cls=IN,ttl=1,payload=ImaRecordA)
-class MammatusResolver(client.Resolver):
-    def filterAnswers(self, message):
-        return ([ImaRR], [], [])
+
+class MammatusResolver(common.ResolverBase):
+    def __init__(self):
+        print "INIT!!"
+        common.ResolverBase.__init__(self)
+        self._waiting = {}
+
+    def _lookup(self, name, cls, type, timeout):
+        print "LOOKUP!!"
+        key = (name, type, cls)
+        waiting = self._waiting.get(key)
+        if waiting is None:
+            self._waiting[key] = []
+            d = defer.fail(IOError("No domain name servers available"))
+            def cbResult(result):
+                for d in self._waiting.pop(key):
+                    d.callback(result)
+                return ([ImaRR], [], [])
+            d.addBoth(cbResult)
+        else:
+            d = defer.Deferred()
+            waiting.append(d)
+        return d
+
 
 verbosity = 0
-resolver = MammatusResolver(servers=[('10.1.1.1', 53)])
+resolver = MammatusResolver()
 f = server.DNSServerFactory(clients=[resolver], verbose=verbosity)
 p = dns.DNSDatagramProtocol(f)
 f.noisy = p.noisy = verbosity
 
 reactor.listenUDP(53, p)
 reactor.listenTCP(53, f)
+print 'Mammatus DNS Active'
 reactor.run()
 
