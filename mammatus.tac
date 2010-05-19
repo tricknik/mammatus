5780ea9b15a8f2448174c5a6a3cb445fa8d15039
Dmytri Kleiner <dk@telekommunisten.net>
2010-05-19 15:09:26 +0200

    from the useful idiot to the running dog lackey, a begining

--- mammatus.tac
+++ mammatus.tac
@@ -5,11 +5,11 @@ Dmytri Kleiner <dk@telekommunisten.net>, 2010
 """
 from twisted.application import service
 try:
-    from mammatus import useful_idiot
+    from mammatus import multimammia 
 except ImportError:
     import sys
     sys.path.append('.')
-    from mammatus import useful_idiot
+    from mammatus import multimammia 
 
 #########
 # All Hail Mammatus, the many breasted goddess
@@ -17,5 +17,5 @@ except ImportError:
 # numerous nipples, and hunger no more.
 ##
 application = service.Application("Mammatus Tracker")
-useful_idiot.attach(application)
+multimammia.attach(application)
 
--- mammatus/multimammia.py
+++ mammatus/multimammia.py
@@ -3,19 +3,25 @@ Mammatus, a DNS-centric HA platform
 
 Dmytri Kleiner <dk@telekommunisten.net>, 2010
 
-The "simplest thing that could possibly work" edition
+The "Running Dog Lackey" edition
 
-This is an initial implementation of the simplest possible
+This is an implementation of the simplest possible
 scenario for Mammatus; exploiting the ubiquity of DNS fail-over 
 to create a high availablity webserver that only redirects a get 
 request to a content delivery network.
+
+This version uses _mammatus TXT and SRV record form
+the domain of the recieved request to find the storage
+nodes.
 """
 
 import os, socket, urlparse
 from twisted.application import service, internet
-from twisted.internet import defer
+from twisted.internet import reactor, defer
+from twisted.internet.task import deferLater
 from twisted.web import server as web_server, resource
-from twisted.names import server as names_server, dns, hosts, common, client
+from twisted.names import server as names_server, dns, common, client
+from random import choice
 
 #########
 # Create a DNS A Record for the IP address of this computer
@@ -30,6 +36,14 @@ def getRecordA():
     IPAddr = s.getsockname()[0]
     return dns.Record_A(IPAddr,'1')
 RECORD_A = getRecordA()
+
+
+#########
+# Dictionary of CDN baseUrls for domains
+# Set when request for a given domain is recieved
+##
+
+BASEURLS = {}
 
 
 #########
@@ -64,7 +78,6 @@ class MammatusIdioticResolver(common.ResolverBase):
 #########
 # What could be a better companion to a DNS Resolver
 # that always claims to be whatever domain you are looking for
-# than an HTTP server that always tells you to go somewhere else?
 # Wharever you want It's OVER HERE!!! 
 # Umm, No it's not. Try over there.
 ##
@@ -73,21 +86,52 @@ class MammatusRedirectToCdn(resource.Resource):
     """
     isLeaf = True
     def render_GET(self, request):
-        url = self.getStorageUrl(request)
-        request.redirect(url)
-        request.finish()
+        def gotFailure(failure):
+            request.write("lookup for " + str(failure.value.message.queries[0].name) + " failed")
+            request.finish()
+        def getZone(uri):
+            domainlevels = uri.split(".")
+            levelcount = len(domainlevels)
+            if levelcount < 2:
+                domainlevels = socket.getfqdn().split(".")
+                levelcount = len(domainlevels)
+            registeredDomain = domainlevels[levelcount-2:levelcount]
+            subdomain = domainlevels[:levelcount-2]
+            subdomain.append("_mammatus")
+            mammatus_key = ".".join(subdomain) 
+            root_zone = ".".join(registeredDomain)
+            return (mammatus_key, root_zone)
+        url = urlparse.urlparse(request.uri).netloc
+        (mammatus_key, root_zone) = getZone(url)
+        d = client.lookupText(".".join((mammatus_key, root_zone)))
+        def gotError(failure):
+            service_fqdn = ".".join(("_mammatus._tcp", root_zone))
+            d = client.lookupService(service_fqdn) 
+            def gotSrvRecord(result):
+                (answer, authority, additional) = result
+                serviceUri = str(answer[0].payload.target)
+                d = client.lookupText(".".join(getZone(serviceUri)))
+                d.addCallbacks(getStorageUrls, gotFailure)
+                return d
+            d.addCallbacks(gotSrvRecord, gotFailure)
+            return d
+        def getStorageUrls(result):
+            (answer, authority, additional) = result
+            if answer and answer[0].payload.data:
+                #request.write(str(answer[0].payload.data[0]))
+                #request.finish()
+                return choice(["http://www.archive.org",])
+            else:
+                return gotError(None)
+        d.addCallbacks(getStorageUrls, gotError)
+        def redirect(url):
+            endpoint =  urlparse.urljoin(url, request.uri)
+            request.redirect(endpoint)
+            request.finish()
+            return None
+        d.addCallback(redirect)
         return web_server.NOT_DONE_YET
-    def getStorageUrl(self, request):
-        """ Return base URL for content delivery network
-        """
-        #########
-        # Special url components can be added here, but for example
-        # we just us a static base url
-        ##
-        cdnUrl = "http://www.archive.org/"
-        requestUrl = request.uri
-        return urlparse.urljoin(cdnUrl, requestUrl)
-        
+
 #########
 # Mammatus is the giver of names, on TCP and UDP.
 ##
