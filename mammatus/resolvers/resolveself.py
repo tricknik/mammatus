d180f5c59627466edc7796413c2b42f469bee946
Dmytri Kleiner <dk@telekommunisten.net>
2010-05-20 14:38:22 +0200

    added packages

---
+++ mammatus/resolvers/resolveself.py
@@ -0,0 +1,57 @@
+"""
+Mammatus, a DNS-centric HA platform 
+
+Dmytri Kleiner <dk@telekommunisten.net>, 2010
+"""
+
+import os, socket, urlparse
+from twisted.application import service, internet
+from twisted.internet import reactor, defer
+from twisted.names import server as names_server, dns, common, client
+
+#########
+# On the surface of it, a DNS resolver which only responds
+# with it's own IP address may seem to be the most idiotic class
+# ever written. But HAHA! For us, the only thing better than
+# a dummy resolver is a useful idiot.
+##
+class MammatusIdioticResolver(common.ResolverBase):
+    """ Respond to all requests with own IP address
+    """
+    def __init__(self):
+        common.ResolverBase.__init__(self)
+        self._waiting = {}
+        #########
+        # Create a DNS A Record for the IP address of this computer
+        # by connecting to some outside place and checking the socket
+        ##
+        def getRecordA():
+            """ Return an A record with external IP address of localhost
+            """
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            # umm, I know google is never, never, never-ever down right?
+            s.connect(('google.com', 0)) 
+            IPAddr = s.getsockname()[0]
+            return dns.Record_A(IPAddr,'1')
+        self.Record_A = getRecordA()
+
+    def _lookup(self, name, cls, type, timeout):
+        key = (name, type, cls)
+        waiting = self._waiting.get(key)
+        if waiting is None:
+            self._waiting[key] = []
+            d = defer.fail(IOError("No domain name servers available"))
+            def cbResult(result):
+                for d in self._waiting.pop(key):
+                    d.callback(result)
+                RR = dns.RRHeader(name=name,type=dns.A,cls=dns.IN,ttl=1,payload=self.Record_A)
+                return ([RR], [], [])
+            d.addBoth(cbResult)
+        else:
+            d = defer.Deferred()
+            waiting.append(d)
+        return d
+
+def getResolver():
+    return MammatusIdioticResolver()
+
---
+++ mammatus/servers/redirect.py
@@ -0,0 +1,74 @@
+"""
+Mammatus, a DNS-centric HA platform 
+
+Dmytri Kleiner <dk@telekommunisten.net>, 2010
+"""
+
+import socket, urlparse
+from twisted.internet import reactor, defer
+from twisted.web import server, resource
+from twisted.names import client
+from random import choice
+
+class MammatusRedirect(resource.Resource):
+    """ Redirect All GET requests to CDN
+    """
+    isLeaf = True
+    def render_GET(self, request):
+        def gotFailure(failure):
+            request.write("lookup for " + str(failure.value.message.queries[0].name) + " failed")
+            request.finish()
+        def getZone(uri):
+            domainlevels = uri.split(".")
+            levelcount = len(domainlevels)
+            # if the request is comming from an not fully qualied
+            # domain name, i.e "localhost", use our own.
+            if levelcount < 2:
+                domainlevels = socket.getfqdn().split(".")
+                levelcount = len(domainlevels)
+            registeredDomain = domainlevels[levelcount-2:levelcount]
+            subdomain = [domainlevels[:levelcount-2].pop(), "_mammatus"]
+            mammatus_key = ".".join(subdomain) 
+            root_zone = ".".join(registeredDomain)
+            return (mammatus_key, root_zone)
+        url = request.getRequestHostname()
+        (mammatus_key, root_zone) = getZone(url)
+        d = client.lookupText(".".join((mammatus_key, root_zone)))
+        def gotError(failure):
+            service_fqdn = ".".join(("_mammatus._tcp", root_zone))
+            d = client.lookupService(service_fqdn) 
+            def gotSrvRecord(result):
+                (answer, authority, additional) = result
+                serviceUri = str(answer[0].payload.target)
+                d = client.lookupText(".".join(getZone(serviceUri)))
+                d.addCallbacks(getStorageUrls, gotFailure)
+                return d
+            d.addCallbacks(gotSrvRecord, gotFailure)
+            return d
+        def getStorageUrls(result):
+            (answer, authority, additional) = result
+            CDNs = []
+            token = "endpoint:"
+            tokenlength = len(token)
+            if answer:
+                for a in answer:
+                    for d in a.payload.data:
+                        for e in str(d).split(";"):
+                                item = e.strip()
+                                if item[:tokenlength] == token:
+                                    CDNs.append(item[tokenlength:])
+                return choice(CDNs)
+            else:
+                return gotError(None)
+        d.addCallbacks(getStorageUrls, gotError)
+        def redirect(url):
+            endpoint =  urlparse.urljoin(url, request.uri)
+            request.redirect(endpoint)
+            request.finish()
+            return None
+        d.addCallback(redirect)
+        return server.NOT_DONE_YET
+
+def getServer():
+    return MammatusRedirect()
+
