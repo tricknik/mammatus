089c699148c705cdcb0bc495546ec667038b56d3
Dmytri Kleiner <dk@telekommunisten.net>
2010-06-09 13:44:06 +0200

    http controller supports reverse proxy

--- controllers/http.py
+++ controllers/http.py
@@ -8,6 +8,7 @@ import socket, urlparse
 from twisted.internet import reactor, defer
 from twisted.internet.task import deferLater
 from twisted.web import server, resource
+from twisted.web.proxy import ReverseProxyResource
 from twisted.names import client
 from random import choice
 
@@ -15,6 +16,8 @@ class MammatusHttpResource(resource.Resource):
     isLeaf = True
     def setModel(self, model):
         self.model = model
+    def setLocalRoot(self, localroot):
+        self.localRoot = localroot)
     def render_GET(self, request):
         config = None
         def error(failure):
@@ -39,17 +42,8 @@ class MammatusHttpResource(resource.Resource):
         d.addCallbacks(direct, error)
         return server.NOT_DONE_YET
 
-class Controller(MammatusHttpResource):
-    def serve(self, request, endpoint, config):
-        pass
-    def proxy(self, request, endpoint, config):
-        pass
-    def redirect(self, request, endpoint, config):
-        target =  urlparse.urljoin(endpoint, request.uri)
-        request.redirect(target)
-        request.finish()
-
-def getController(model):
+def getController(model, localroot="/srv/mammatus"):
     controller = Controller()
     controller.setModel(model)
+    controller.setLocalRoot(localroot)
     return controller
